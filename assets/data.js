b545b99d30dc70bc160c05b3fe88af6c7c7c0922
swathicherala <swathicherala96@gmail.com>
2022-09-07 18:19:45 +0530

    second commit

--- assets/data.js
+++ assets/data.js
@@ -8,19 +8,19 @@ export const largeCardSections = [
           id: 19,
           title: "Toronto's Portable 2L Fire Extinguisher",
           snippet: "This fire extinguisher saved homes from completely burning down. People that used it only had 98% of their homes burned down.",
-          image: "homebook.jpg"
+          image: "homebook1.jpeg"
         },
         {
           id: 20,
           title: "Empty Super Red Fire Extinguisher",
           snippet: "This is one of the most red fire extinguishers you will ever see. Super clean. It's pretty much useless otherwise.",
-          image: "homebook.jpg"
+          image: "homebook2.jpeg"
         },
         {
           id: 21,
           title: "Fire Extinguisher, Perfect for Pools",
           snippet: "If you ever find that your pool is on fire then this is the fire extinguisher for you. It has a 100% success rate.",
-          image: "homebook.jpg"
+          image: "homebook3.jpeg"
         }
       ]
     },
@@ -33,19 +33,19 @@ export const largeCardSections = [
           id: 22,
           title: "Vintage 1864 Fire Extinguisher",
           snippet: "This is hands down the worst fire extinguisher you can use in a crisis. The radiation will kill you if the fire doesn't",
-          image: "homebook.jpg"
+          image: "homebook4.jpeg"
         },
         {
           id: 23,
           title: "Pure Silver & Gold Fire Extinguisher",
           snippet: "This one is made out of 50k worth of pure silver and white gold. The metal choice was a poor for functionality, but was great for style!",
-          image: "homebook.jpg"
+          image: "homebook5.jpeg"
         },
         {
           id: 24,
           title: "Two in One Fire Extinguisher",
           snippet: "This is the first ever two in one fire extinguisher. Good if both you and your friend's houses are burining down",
-          image: "homebook.jpg"
+          image: "homebook6.jpeg"
         }
       ]
     }
@@ -57,51 +57,51 @@ export const largeCardSections = [
         cards: [
             {
               id: 1,
-              image: "homebook.jpg"
+              image: "homebook1.jpeg"
             },
             {
               id: 2,
-              image: "homebook.jpg"
+              image: "homebook2.jpeg"
             },
             {
               id: 3,
-              image: "homebook.jpg"
+              image: "homebook3.jpeg"
             },
             {
               id: 4,
-              image: "homebook.jpg"
+              image: "homebook4.jpeg"
             },
             {
               id: 5,
-              image: "homebook.jpg"
+              image: "homebook5.jpeg"
             },
             {
               id: 6,
-              image: "homebook.jpg"
+              image: "homebook6.jpeg"
             },
             {
               id: 7,
-              image: "homebook.jpg"
+              image: "hombook7.jpeg"
             },
             {
               id: 8,
-              image: "homebook.jpg"
+              image: "homebook8.jpeg"
             },
             {
               id: 9,
-              image: "homebook.jpg"
+              image: "homebook9.jpeg"
             },
             {
               id: 10,
-              image: "homebook.jpg"
+              image: "homebook10.jpeg"
             },
             {
               id: 11,
-              image: "homebook.jpg"
+              image: "homebook11.jpeg"
             },
             {
               id: 12,
-              image: "homebook.jpg"
+              image: "homebook12.jpeg"
             }
         ]
     },
@@ -111,51 +111,51 @@ export const largeCardSections = [
       cards: [
           {
             id: 13,
-            image: "homebook.jpg"
+            image: "homebook13.jpeg"
           },
           {
             id: 14,
-            image: "homebook.jpg"
+            image: "homebook14.jpeg"
           },
           {
             id: 15,
-            image: "homebook.jpg"
+            image: "homebook15.jpeg"
           },
           {
             id: 16,
-            image: "homebook.jpg"
+            image: "homebook16.jpeg"
           },
           {
             id: 17,
-            image: "homebook.jpg"
+            image: "homebook16.jpeg"
           },
           {
             id: 18,
-            image: "homebook.jpg"
+            image: "homebook17.jpeg"
           },
           {
             id: 19,
-            image: "homebook.jpg"
+            image: "homebook18.jpeg"
           },
           {
             id: 20,
-            image: "homebook.jpg"
+            image: "homebook19.jpeg"
           },
           {
             id: 21,
-            image: "homebook.jpg"
+            image: "homebook20.jpeg"
           },
           {
             id: 22,
-            image: "homebook.jpg"
+            image: "homebook21.jpeg"
           },
           {
             id: 23,
-            image: "homebook.jpg"
+            image: "homebook22.jpeg"
           },
           {
             id: 24,
-            image: "homebook.jpg"
+            image: "homebook23.jpeg"
           }
       ]
   }
